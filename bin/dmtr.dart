5f0c18ea05169bc6c22ca0913e3e8adb0d5cdd1b
yvs <VSYakovetsky@gmail.com>
2023-07-04 02:10:44 +0300

    up to a simple report

--- bin/dmtr.dart
+++ bin/dmtr.dart
@@ -1,60 +1,101 @@
-import 'dart:io';
-import 'package:intl/intl.dart';
+
+import 'package:sprintf/sprintf.dart';
 import 'package:dart_ping/dart_ping.dart';
 
 const maxTtl = 15;
-final frtt = NumberFormat("###.0");
+const hostnameLen = 30;
+final hopHeader = sprintf('%-*s\t%s\t%s\t%s', [hostnameLen, 'Host', 'Sent', 'Rcvd', 'Last[msec]']);
 
 int hops = maxTtl;
 int timeout = 1; // 1sec
 
+typedef TsUsec = ({int sec, int usec}); // timestamp: sec, usec
+
 class Hop {
-  int snt = 0;
+  int sent = 0;
   int rcvd = 0;
   int last = 0; // in usec
   String? addr;
   String? name;
+  //
   Ping? ping;
-//  Hop();
+  int disc = -1; // to avoid dups at calculation of 'sent'
+  TsUsec? ts;    // timestamp of timeouted response
+  @override
+  String toString() {
+    final hop = (name ?? addr) ?? '';
+    String l = (last > 0) ? sprintf("%.1f", [last / 1000]) : '-';
+    return sprintf('%-*s\t%d\t%d\t%s', [hostnameLen, hop, sent, rcvd, l]);
+  }
 }
 
 List<Hop> stat = List<Hop>.generate(maxTtl, (_) => Hop());
 
-Future <void> pingHop(String host, int i, int count) async {
-  int ttl = i + 1;
-  stat[i].ping = Ping(host, count: count, ttl: ttl, timeout: timeout, timing: true, dns: true);
-  var p = stat[i].ping;
-  if (p != null) {
-    await for (final ev in p.stream) {
-      var re = ev.response;
-      if (re != null) {
-        print("reply $re on ttl=$ttl (ndx=$i)");
-        if (re.ttl != null) { // use re.ttl as a marker of successful ping
-          if (hops > ttl) {
+TsUsec parseTs(String s) {
+  var a = s.split('.');
+  int sec = 0, usec = 0;
+  if (a.length == 2) { sec = int.parse(a[0]); usec = int.parse(a[1]); }
+  if (a.length == 1) sec = int.parse(a[0]);
+  return (sec: sec, usec: usec);
+}
+
+void setHopINSR(int ndx, PingResponse re) {
+   if (re.ip != null) stat[ndx].addr = re.ip;
+   if (re.name != null) stat[ndx].name = re.name;
+   stat[ndx].sent++;
+   stat[ndx].rcvd++;
+   if (re.seq != null) stat[ndx].disc = re.seq!;
+}
+
+Future <void> readEvents(int ttl, var stream) async {
+  await for (final ev in stream) {
+    var re = ev.response;
+    if (re != null) {
+      int ndx = ttl - 1;
+//      print("reply $re on ttl=$ttl (ndx=$ndx)");
+      switch (re?.status) {
+        case ReStatus.success:
+          if ((re.ttl != null) && (hops > ttl)) {
             hops = ttl; // stop pings at this ttl
-            for (int j = hops; j < maxTtl; j++) { stat[i].ping?.stop(); }
+            for (int i = hops; i < maxTtl; i++) { stat[i].ping?.stop(); }
           }
-        }
+          if (re.time != null) stat[ndx].last = re.time?.inMicroseconds ?? 0;
+          setHopINSR(ndx, re);
+        case ReStatus.discard:
+          setHopINSR(ndx, re);
+          if ((re.ts != null) && (stat[ndx].ts != null)) {
+//            print("prev=${stat[ndx].ts} curr=${parseTs(re.ts)}");
+            TsUsec tu = parseTs(re.ts);
+            stat[ndx].last = ((tu.sec - stat[ndx].ts!.sec) * 1000000 + (tu.usec - stat[ndx].ts!.usec)).toInt();
+          }
+        case ReStatus.timeout:
+          if (stat[ndx].disc != re.seq) stat[ndx].sent++;
+          stat[ndx].ts = (re.ts != null) ? parseTs(re.ts) : null; // keep it for possible future discard
       }
     }
   }
 }
 
 
+Future <void> pingHops(String host, int count) async {
+  List<Future<void>> readers = [];
+  for (int i = 0; i < maxTtl; i++) {
+    int ttl = i + 1;
+    stat[i].ping = Ping(host, count: count, ttl: ttl, timeout: timeout, timing: true, dns: true);
+    var p = stat[i].ping;
+    if (p != null) readers.add(readEvents(ttl, p.stream));
+  }
+  await Future.wait(readers).then((_) => {/*print("finish")*/});
+}
+
+
 main() async {
   var count = 5;
   var host = 'google.com';
-  final timer = Stopwatch()..start();
-  //
-  for (int i = 0; i < maxTtl; i++) { // start all pings upto maxTtl
-    pingHop(host, i, count);
-  }
-  sleep(Duration(seconds: 10));
-  timer.stop();
-  print("elapsed[msec]: ${timer.elapsedMilliseconds}");
-  print("Hop#\tSnt\tRcvd\tLast[msec]");
+  await pingHops(host, count);
+  print(sprintf('    %s', [hopHeader]));
   for (int i = 0; i < hops; i++) {
-    print("$i\t${stat[i].snt}\t${stat[i].rcvd}\t${frtt.format(stat[i].last / 1000)}");
+    print(sprintf('%2d. %s', [i + 1, stat[i]]));
   }
 }
 
