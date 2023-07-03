af8f051fa05ded8eda2e465a013b13844c356831
yvs <VSYakovetsky@gmail.com>
2023-07-03 03:39:27 +0300

    create Ping object per hop

--- bin/dmtr.dart
+++ bin/dmtr.dart
@@ -1,64 +1,60 @@
 import 'dart:io';
+import 'package:intl/intl.dart';
 import 'package:dart_ping/dart_ping.dart';
 
-int maxTtl = 30;
+const maxTtl = 15;
+final frtt = NumberFormat("###.0");
+
+int hops = maxTtl;
 int timeout = 1; // 1sec
 
 class Hop {
   int snt = 0;
   int rcvd = 0;
-  int last = 0;
+  int last = 0; // in usec
   String? addr;
   String? name;
+  Ping? ping;
 //  Hop();
 }
 
 List<Hop> stat = List<Hop>.generate(maxTtl, (_) => Hop());
 
-Future<bool> cyclePing() async {
-  bool goal = false;
-  for (int i = 0; i < maxTtl; i++) {
-    int ttl = i + 1;
-    final timer = Stopwatch()..start(); // for discarded replies
-    var ping = Ping('google.com', timeout: timeout, count: 1, dns: true, ttl: ttl);
-    stat[i].snt += 1;
-    await for (final event in ping.stream) { // loop just in case for count>1
-      timer.stop();
-      var re = event.response;
+Future <void> pingHop(String host, int i, int count) async {
+  int ttl = i + 1;
+  stat[i].ping = Ping(host, count: count, ttl: ttl, timeout: timeout, timing: true, dns: true);
+  var p = stat[i].ping;
+  if (p != null) {
+    await for (final ev in p.stream) {
+      var re = ev.response;
       if (re != null) {
-        if (re.time != null) {
-          if (maxTtl > ttl) maxTtl = ttl; // stop pings at this ttl
-          if (!goal) goal = true;
-          stat[i].rcvd++;
-          stat[i].last = re.time?.inMilliseconds ?? 0;
-        } else {
-          stat[i].last = timer.elapsedMilliseconds;
+        print("reply $re on ttl=$ttl (ndx=$i)");
+        if (re.ttl != null) { // use re.ttl as a marker of successful ping
+          if (hops > ttl) {
+            hops = ttl; // stop pings at this ttl
+            for (int j = hops; j < maxTtl; j++) { stat[i].ping?.stop(); }
+          }
         }
-        if (re.ip != null) stat[i].addr = re.ip;
-        if (re.name != null) stat[i].name = re.name;
       }
     }
-    if (timer.isRunning) timer.stop();
-    if (goal) break;
   }
-  return goal;
 }
 
+
 main() async {
-  int count = 10;
-  for (int i = 0; i < count; i++) {
-//    print("tm: ${DateTime.now().millisecondsSinceEpoch}");
-    final timer = Stopwatch()..start();
-    bool re = await cyclePing();
-    timer.stop();
-    print('cycle=${i + 1} maxTtl=$maxTtl: reached=$re duration=${timer.elapsedMilliseconds}'); 
-    if (!re) maxTtl++;
-    int spareTime = timeout * 1000 - timer.elapsedMilliseconds;
-    if (spareTime > 0) sleep(Duration(milliseconds: spareTime));
+  var count = 5;
+  var host = 'google.com';
+  final timer = Stopwatch()..start();
+  //
+  for (int i = 0; i < maxTtl; i++) { // start all pings upto maxTtl
+    pingHop(host, i, count);
   }
+  sleep(Duration(seconds: 10));
+  timer.stop();
+  print("elapsed[msec]: ${timer.elapsedMilliseconds}");
   print("Hop#\tSnt\tRcvd\tLast[msec]");
-  for (int i = 0; i < maxTtl; i++) {
-    print("$i\t${stat[i].snt}\t${stat[i].rcvd}\t${stat[i].last}");
+  for (int i = 0; i < hops; i++) {
+    print("$i\t${stat[i].snt}\t${stat[i].rcvd}\t${frtt.format(stat[i].last / 1000)}");
   }
 }
 
